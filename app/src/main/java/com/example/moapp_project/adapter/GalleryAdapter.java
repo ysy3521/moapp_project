2622089ae6706ded569b26c3ac41bd3b3e8d7879
ysy3521 <127924018+ysy3521@users.noreply.github.com>
2023-06-24 16:12:35 +0900

    g

--- app/src/main/java/com/example/moapp_project/adapter/GalleryAdapter.java
+++ app/src/main/java/com/example/moapp_project/adapter/GalleryAdapter.java
@@ -2,13 +2,10 @@ package com.example.moapp_project.adapter;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
